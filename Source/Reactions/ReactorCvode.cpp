d0d6e535b9b5771a13891c2a878966813eae683c
Ashesh <ashesh.sharma@hpe.com>
2024-11-15 14:41:44 -0600

    Port to HIP

--- Source/Reactions/ReactorCvode.cpp
+++ Source/Reactions/ReactorCvode.cpp
@@ -1737,26 +1737,26 @@ ReactorCvode::cF_RHS(
 
   /////////////////////////////////////////////////////////////////////
 
-  // {
-  //   const int block_size = 256;
-  //   utils::fKernelSpecBase_CUDA<Ordering>
-  //     <<<(ncells + block_size - 1) / block_size, block_size>>>(
-  //       ncells, dt_save, yvec_d, ydot_d, rhoe_init, rhoesrc_ext, rYsrc_ext);
-  // }
-  // cudaDeviceSynchronize();
+  {
+    const int block_size = 256;
+    utils::fKernelSpecBase_HIP<Ordering>
+      <<<(ncells + block_size - 1) / block_size, block_size>>>(
+        ncells, dt_save, yvec_d, ydot_d, rhoe_init, rhoesrc_ext, rYsrc_ext);
+  }
+  hipDeviceSynchronize();
 
   /////////////////////////////////////////////////////////////////////
 
-  {
-    const int nthreads_per_block = 64; // multiple of warpSize rounded up,
-                                       // based on number of species
-    dim3 block(nthreads_per_block);
-    dim3 grid(ncells); // 1 cell is assigned 1 block - could be inefficent
-                       // chemsitry model with less number of species
-    utils::fKernelSpec_CUDAReg<Ordering><<<grid, block>>>(
-      ncells, dt_save, yvec_d, ydot_d, rhoe_init, rhoesrc_ext, rYsrc_ext);
-  }
-  cudaDeviceSynchronize();
+  // {
+  //   const int nthreads_per_block = 64; // multiple of warpSize rounded up,
+  //                                      // based on number of species
+  //   dim3 block(nthreads_per_block);
+  //   dim3 grid(ncells); // 1 cell is assigned 1 block - could be inefficent
+  //                      // chemsitry model with less number of species
+  //   utils::fKernelSpec_HIPReg<Ordering><<<grid, block>>>(
+  //     ncells, dt_save, yvec_d, ydot_d, rhoe_init, rhoesrc_ext, rYsrc_ext);
+  // }
+  // hipDeviceSynchronize();
 
   ///////////////////////////////////////////////////////////////////////
 
@@ -1766,23 +1766,23 @@ ReactorCvode::cF_RHS(
     dim3 block(nthreads_per_block);
     dim3 grid(ncells); // 1 cell is assigned 1 block - could be inefficent for
                        // chemsitry model with less number of species
-    utils::fKernelSpecOpt_CUDA<Ordering><<<grid, block>>>(
+    utils::fKernelSpecOpt_HIP<Ordering><<<grid, block>>>(
       ncells, dt_save, yvec_d, ydot_d_opt_ptr, rhoe_init, rhoesrc_ext,
       rYsrc_ext);
   }
-  cudaDeviceSynchronize();
+  hipDeviceSynchronize();
 
   ///////////////////////////////////////////////////////////////////////
 
-  N_VCopyFromDevice_Cuda(ydot_in);
-  N_VCopyFromDevice_Cuda(ydot_d_opt);
-  amrex::Real* ydot_h_base = N_VGetHostArrayPointer_Cuda(ydot_in);
-  amrex::Real* ydot_h_opt = N_VGetHostArrayPointer_Cuda(ydot_d_opt);
+  N_VCopyFromDevice_Hip(ydot_in);
+  N_VCopyFromDevice_Hip(ydot_d_opt);
+  amrex::Real* ydot_h_base = N_VGetHostArrayPointer_Hip(ydot_in);
+  amrex::Real* ydot_h_opt = N_VGetHostArrayPointer_Hip(ydot_d_opt);
 
   amrex::Real base = 0.0;
   amrex::Real opt = 0.0;
   amrex::Real diff = 0.0;
-  amrex::Real tol = 1e-16;
+  amrex::Real tol = 1e-3;
   for (int i = 0; i < ncells; i++) {
     for (int n = 0; n < (NUM_SPECIES + 1); n++) {
       base = ydot_h_base[utils::vec_index<Ordering>(n, i, ncells)];
